95b50f0b50982cd607d284f6d6e254a2a84563d2
Grenco <graemesmyrl@gmail.com>
2023-09-02 17:34:50 +0100

    Added class for extracting data from a dicom file

---
+++ Engine/Dicom/dicomReader.py
@@ -0,0 +1,24 @@
+
+from pathlib import Path
+import pydicom as dicom
+import numpy as np
+
+
+class DicomReader:
+    """Used for extracting information from a given dicom file"""
+    def __init__(self):
+        self.SUPPORTED_FILE_TYPES = [".dcm"]
+
+    def get_image(self, path: Path) -> np.ndarray:
+        """Extracts the image from the given file path"""
+        dicom_file = self.__read(path)
+        return dicom_file.pixel_array
+
+    def __read(self, path: Path) -> dicom.FileDataset:
+        """Read a dicom file at a given path"""
+        ext: str = path.suffix
+        if ext not in self.SUPPORTED_FILE_TYPES:
+            raise ValueError(f"File extension \"{ext}\" is not supported. "
+                             f"Supported file types: {self.SUPPORTED_FILE_TYPES}. "
+                             f"Path: {path}")
+        return dicom.dcmread(path)
