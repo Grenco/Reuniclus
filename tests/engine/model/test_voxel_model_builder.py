117a9337f5450d40447e4b452ab09f114197dad5
Grenco <graemesmyrl@gmail.com>
2023-09-11 17:50:04 +0100

    Added some unit tests for the voxel model bulider

---
+++ tests/engine/model/test_voxel_model_builder.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+from engine.model.voxel_model_builder import VoxelModelBuilder
+import unittest
+
+
+class TestVoxelModelBuilder(unittest.TestCase):
+
+    def setUp(self):
+        self.sut = VoxelModelBuilder(5)
+
+    def test_add_layer(self):
+        # Act
+        self.sut.add_layer(np.array([[1, 0], [1, 0]]))
+        # Assert
+        self.assertEqual(1, self.sut.layer)
+
+    def test_build(self):
+        # Arrange
+        self.__build_test_model()
+        # Act
+        result: np.ndarray = self.sut.build()
+        # Assert
+        self.assertEqual((2, 2, 5), result.shape)
+
+    def __build_test_model(self):
+        self.sut.add_layer(np.array([[1, 0], [0, 1]]))
+        self.sut.add_layer(np.array([[1, 0], [0, 1]]))
+        self.sut.add_layer(np.array([[1, 0], [0, 1]]))
+        self.sut.add_layer(np.array([[1, 0], [0, 1]]))
+        self.sut.add_layer(np.array([[1, 0], [0, 1]]))
+
+
+if __name__ == '__main__':
+    unittest.main()
